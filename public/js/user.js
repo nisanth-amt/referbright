e46bea820e372d9acb2bca5c4a9e3bcf4ce5cc0b
Nisanth Sojan <nisanth@amt.in>
2013-10-16 13:53:45 +0530

    added to database

--- public/js/user.js
+++ public/js/user.js
@@ -1,22 +1,23 @@
-$( document ).ready(function() {
-   console.info('ready');    
-   // TODO: validate
-   var formData = {}; //Array
+$(document).ready(function () {
+    console.info('ready');
+    // TODO: validate
+    var formData = {
+        name: $("#yoga_instructor").val(),
+        number: $("#yoga_instructor_in").val()
+    }; //Array
 
-   $("#professional").on('click', function() {
-   $.ajax({
-       url : "./profession",
-       type: "POST",
-       data : formData,
-       success: function(data, textStatus, jqXHR)
-       {
-        //data - response from server
-          $("#mainContainer > div").replaceWith(data);
-       },
-       error: function (jqXHR, textStatus, errorThrown)
-       {
- 
-       }
-    });
+    $("#professional").on('click', function () {
+        $.ajax({
+            url: "./profession",
+            type: "POST",
+            data: formData,
+            success: function (data, textStatus, jqXHR) {
+                //data - response from server
+                $("#mainContainer > div").replaceWith(data);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+
+            }
+        });
     });
 });
--- routes/user.js
+++ routes/user.js
@@ -1,25 +1,36 @@
-var mysql      = require('mysql'),
+var mysql = require('mysql'),
+    DATABASE = 'referbright',
     connection = mysql.createConnection({
-        host     : 'localhost',
-        user     : 'remote',
-        password : 'test321',
+        port:3306,
+        host: '192.168.2.2',
+        user: 'remote',
+        password: 'test123'
     });
 
 
 exports.index = function (req, res) {
     res.render("index");
-}
+};
 
 exports.addProfession = function (req, res) {
-   // TODO insert to db
+    // TODO insert to db
+    connection.connect();
+    connection.query('use ' + DATABASE);
+    connection.query('insert into instructors (name, number) values ("' + req.body.name + '", "' + req.body.number + '")',
+        function insertCb(err, results, fields) {
+            if (err) throw err;
 
-   res.render("profile");
-}
+            res.render("profile");
+            connection.end();
+        });
+
+
+};
 
 exports.addProfile = function (req, res) {
 
-}
+};
 
 exports.activate = function (req, res) {
 
-}
+};
